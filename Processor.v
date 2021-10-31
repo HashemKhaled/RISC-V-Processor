02cae37b890fa4c923f9ef0ffe74a14d5ab4857a
Hashem Khaled <64085426+HashemKhaled@users.noreply.github.com>
2021-10-31 22:51:48 +0200

    Update Processor.v

--- Processor.v
+++ Processor.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/31/2021 09:35:45 PM
-// Design Name: 
-// Module Name: Processor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Processor(input clk, reset, ssd_clk, input[1:0] ledSel, input[3:0] ssdSel, output reg[15:0] LEDs, output [3:0] Anode,
  output [6:0] LED_out);
  wire [31:0] instruction;
@@ -33,13 +11,29 @@ module Processor(input clk, reset, ssd_clk, input[1:0] ledSel, input[3:0] ssdSel
  wire [31:0] Immediate;
  wire [3:0] ALUSelection;
  wire [31:0] ALUOutput;
- wire ZF, CF, VF, SF;
+ wire cf, zf, vf, sf;
  wire [31:0] mem_data_out;
  reg [12:0] ssdOut;
+ wire branch_signal;
+ 
+ // PC +4 adder
+ assign PC_plus_4 = PC + 4;
+ assign PC_plus_imm = PC + Immediate;
  
  InstMem inst(.addr(PC), .data_out(instruction));
  control_unit cu(instruction[6:0], branch, memRead, memToReg, memWrite, ALUSrc, regWrite, ALUOp, memOffset, unsignedFlag, PC_mux);
+ RegFile RF(clk, reset, instruction[19:15], instruction[24:20], instruction[11:7], write_data, regWrite, read_data1, read_data2);
+ rv32_ImmGen IG(instruction, Immediate);
+ n_bit_mux mux1(read_data2, Immediate, ALUSrc, ALU_input2);
+ ALU_control_unit ALUCU(ALUOp, instruction[14:12], instruction[30], ALUSelection);
+ prv32_ALU ALU(a.(read_data1), b.(ALU_input2), r.(ALUOutput), cf.(cf), zf.(zf), vf.(vf), sf.(sf), alufn.(ALUSelection));
+ DataMem DM(clk, memRead, memWrite, memOffset, unsignedFlag, ALUOutput, read_data2, mem_data_out);
+ rd_mux rd_input_data(ALUOutput, mem_data_out, PC_plus_4, Immediate, PC_plus_imm, memToReg, write_data);
+ branch_control bc(instruction[14:12], zf, cf, vf, sf, branch_signal);
+ PC_mux nextPC(PC_plus_4, PC_plus_imm, ALUOutput, branch&branch_signal, PC_mux, next_PC);
+ n_bit_reg_file regPC(clk, reset, 1'b1, next_PC , PC);
  
  
+
  
 endmodule
