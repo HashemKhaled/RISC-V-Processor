37c0c1c5e1ff18b882fc7f6e349cf54b65d96af7
Basant Elhussein Abdelaal <63318134+Basant-Elhussein@users.noreply.github.com>
2021-10-31 22:36:52 +0200

    Create branch_control.v

---
+++ branch_control.v
@@ -0,0 +1,18 @@
+// file: Branch_control.v
+// author: @basantelhussein
+
+`timescale 1ns/1ns
+`include "defines.v"
+
+module branch_control(input [2:0] funct3, input zf, cf, vf, sf, output reg sig);
+always@(*)begin
+  if(funct3 == `BR_BEQ) sig = zf;
+  else if(funct3 == `BR_BNE) sig = (!zf);
+  else if(funct3 == `BR_BLT) sig = (sf != vf);
+  else if(funct3 == `BR_BGE) sig = (sf == vf);
+  else if(funct3 == `BR_BLTU) sig = (!cf);
+  else if(funct3 == `BR_BGEU) sig = (cf);
+  else sig = 0;
+end
+
+endmodule
