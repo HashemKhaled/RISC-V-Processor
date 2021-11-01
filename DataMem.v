143a88e85475605e1843a8b47a0e7b56561b86af
Hashem Khaled <64085426+HashemKhaled@users.noreply.github.com>
2021-11-01 14:08:52 +0200

    Update DataMem.v

--- DataMem.v
+++ DataMem.v
@@ -33,20 +33,20 @@ module DataMem
             data_out[23:16] = mem[addr+2];
             data_out[31:24] = mem[addr+3];
         end
-        if(memOffset == 3'b010) begin
+        else if(memOffset == 3'b010) begin
             data_out[7:0] = mem[addr];
             data_out[15:8] = mem[addr+1];
             if(unsignedFlag) data_out[31:16] = 16'b0;
             else data_out[31:16] = {16{data_out[15]}};
         end
-    end
-        if(memOffset == 3'b010) begin
+        else if(memOffset == 3'b001) begin
             data_out[7:0] = mem[addr];
             if(unsignedFlag) data_out[31:16] = 24'b0;
             else data_out[31:16] = {24{data_out[7]}};
         end
+    end
  end
- always@(posedge clk)begin
+ always@(*)begin
     if(MemWrite) begin
          if(memOffset == 3'b100) begin
              mem[addr] = data_in[7:0];
@@ -54,26 +54,34 @@ module DataMem
              mem[addr+2] = data_in[23:16];
              mem[addr+3] = data_in[31:24];
          end
-         if(memOffset == 3'b010) begin
+         else if(memOffset == 3'b010) begin
              mem[addr] = data_in[7:0];
              mem[addr+1] = data_in[15:8];
          end
-     end
-         if(memOffset == 3'b010) begin
+         else if(memOffset == 3'b001) begin
              mem[addr] = data_in[7:0];
          end
+     end
  end
  
  
   
   
   // Data Mem intiilization for experiment 2
-  /*
+  
   initial begin
-    mem[0]=32'd10;
-    mem[1]=32'd7; 
-    mem[2]=32'd1;
-    end
-  */
+  // mem[0-3] = 100
+    mem[0] = 8'h64;
+    mem[1] = 8'h00;
+    mem[2] = 8'h00;
+    mem[3] = 8'h00;
+    
+  // mem[4-7] = 200
+    mem[4] = 8'hC8;
+    mem[5] = 8'h00;
+    mem[6] = 8'h00;
+    mem[7] = 8'h00;   
+  end
+  
   
 endmodule
