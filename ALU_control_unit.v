5a0c411ab0d3010b7cb5e4a36c2dff1afcfb566f
Basant Elhussein Abdelaal <63318134+Basant-Elhussein@users.noreply.github.com>
2021-11-21 21:59:19 +0200

    Update ALU_control_unit.v

--- ALU_control_unit.v
+++ ALU_control_unit.v
@@ -4,7 +4,7 @@
 `timescale 1ns/1ns
 `include "defines.v"
 
-module ALU_control_unit(input [1:0] ALUOp, input [2:0] funct3, input inst30, output reg [3:0] ALUSelection);
+module ALU_control_unit(input [1:0] ALUOp, input [2:0] funct3, input inst25, input inst30, output reg [4:0] ALUSelection);
 
 always@(*) begin
   if(ALUOp == 2'b00) begin
@@ -23,17 +23,30 @@ always@(*) begin
     else ALUSelection = `ALU_PASS;
   end
   else if(ALUOp == 2'b10) begin
-      if(funct3 == 3'b000 && inst30 == 0) ALUSelection = `ALU_ADD;
-      else if(funct3 == 3'b000 && inst30 == 1) ALUSelection = `ALU_SUB;
-      else if(funct3 == 3'b001) ALUSelection = `ALU_SLL;
-      else if(funct3 == 3'b010) ALUSelection = `ALU_SLT;
-      else if(funct3 == 3'b011) ALUSelection = `ALU_SLTU;
-      else if(funct3 == 3'b100) ALUSelection = `ALU_XOR;
-      else if(funct3 == 3'b101 && inst30 == 1'b0) ALUSelection = `ALU_SRL;
-      else if(funct3 == 3'b101 && inst30 == 1'b1) ALUSelection = `ALU_SRA;
-      else if(funct3 == 3'b110) ALUSelection = `ALU_OR;
-      else if(funct3 == 3'b111) ALUSelection = `ALU_AND;   
-   end
+      //M Extenstion
+      if(inst25) begin
+        if(funct3 ==  3'b000)  ALUSelection = `ALU_MUL;
+        else if(funct3 == 3'b001) ALUSelection = `ALU_MULH;
+        else if(funct3 == 3'b010) ALUSelection = `ALU_MULHSU;
+        else if(funct3 == 3'b011) ALUSelection = `ALU_MULHU;
+        else if(funct3 == 3'b100) ALUSelection = `ALU_DIV;
+        else if(funct3 == 3'b101) ALUSelection = `ALU_DIVU;
+        else if(funct3 == 3'b110) ALUSelection = `ALU_REM;
+        else if(funct3 == 3'b111) ALUSelection = `ALU_REMU;
+      end
+      else begin
+        if(funct3 == 3'b000 && inst30 == 0) ALUSelection = `ALU_ADD;
+        else if(funct3 == 3'b000 && inst30 == 1) ALUSelection = `ALU_SUB;
+        else if(funct3 == 3'b001) ALUSelection = `ALU_SLL;
+        else if(funct3 == 3'b010) ALUSelection = `ALU_SLT;
+        else if(funct3 == 3'b011) ALUSelection = `ALU_SLTU;
+        else if(funct3 == 3'b100) ALUSelection = `ALU_XOR;
+        else if(funct3 == 3'b101 && inst30 == 1'b0) ALUSelection = `ALU_SRL;
+        else if(funct3 == 3'b101 && inst30 == 1'b1) ALUSelection = `ALU_SRA;
+        else if(funct3 == 3'b110) ALUSelection = `ALU_OR;
+        else if(funct3 == 3'b111) ALUSelection = `ALU_AND; 
+     end  
+  end
    else begin
     ALUSelection = `ALU_SUB;
    end
